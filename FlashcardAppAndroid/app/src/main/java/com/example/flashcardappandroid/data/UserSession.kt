86e238885d4069e0e064409f26ee87217414587b
vuphan525 <phanvucao2807@gmail.com>
2025-07-01 23:27:12 +0700

    feat(ui): fix ui

--- FlashcardAppAndroid/app/src/main/java/com/example/flashcardappandroid/data/UserSession.kt
+++ FlashcardAppAndroid/app/src/main/java/com/example/flashcardappandroid/data/UserSession.kt
@@ -6,6 +6,6 @@ object UserSession {
     var shouldReloadProfile: Boolean = false
     var deckList: List<DeckResponse>? = null
     var shareddeckList: List<DeckResponse>? = null
-    var statLearnedPercent: String? = null
-    var statDailyAverage: String? = null
+    var statLearnedPercent: String? = 0.toString()
+    var statDailyAverage: String? = 0.toString()
 }
